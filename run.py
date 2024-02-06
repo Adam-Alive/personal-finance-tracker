23764d6704f366c95f7c92f31a434506125deffe
Adam Alive <adamalive.email@gmail.com>
2024-02-06 09:31:39 +0000

    Add code to test data input by user

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Personal Finance Tracker
+
+income = input('Monthly income: ')
+print('Now enter the amount you spend each month on: ')
+rent_mortgage  = input('Rent or mortgage: ')
+gas = input('Gas: ')
+electric = input('Electric: ')
+phone = input('Phone: ')
+food = input('Food: ')
+other = input('Other item not listed: ')
+
+print('Thank you for completing your entries… \n')
+print('Your financial summary will soon follow… \n')
+
